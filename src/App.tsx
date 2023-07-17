5c6511b9edc9917baa764e468034937ace9bf212
Sam Marxz <sammarxzz@gmail.com>
2023-07-17 16:01:07 -0300

    (feature) add and configre styled-components with a theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "styled-components"
+
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/globals"
+
+
 function App() {
   return (
-    <div>Hello World</div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
   )
 }
 
